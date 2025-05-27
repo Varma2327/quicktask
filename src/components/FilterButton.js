b98bda23bcb726a6049b8f5bb2552c78adf14e71
Varma2327 <varma.svs23@gmail.com>
2025-05-27 17:20:17 -0400

    Update FilterButton.js

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 function FilterButtons({ filter, setFilter }) {
+  const filters = ["All", "Active", "Completed"];
 
   return (
-  <div className="filter-buttons">
-  {["All", "Active", "Completed"].map((f) => (
-    <button
-      key={f}
-      onClick={() => setFilter(f)}
-      className={filter === f ? "active" : ""}
-    >
-      {f}
-    </button>
-  ))}
-</div>
-
+    <div className="filter-buttons">
+      {filters.map((f) => (
+        <button
+          key={f}
+          className={filter === f ? "active" : ""}
+          onClick={() => setFilter(f)}
+        >
+          {f}
+        </button>
+      ))}
+    </div>
   );
 }
 
