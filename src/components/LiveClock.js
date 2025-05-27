00d2e2cb5cdb3054cba5f7cc7de339e8a7b545b5
Varma2327 <varma.svs23@gmail.com>
2025-05-27 16:26:22 -0400

    Create LiveClock.js
    Added Live Clock

---
+++ src/components/LiveClock.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+function LiveClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div style={{
+      position: "fixed",
+      top: "20px",
+      right: "30px",
+      fontSize: "20px",
+      fontWeight: "600",
+      color: "#ffffffcc",
+      textShadow: "1px 1px 4px rgba(0,0,0,0.7)"
+    }}>
+      {time.toLocaleTimeString()}
+    </div>
+  );
+}
+
+export default LiveClock;
